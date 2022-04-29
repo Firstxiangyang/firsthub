98c829416b02e4589172420344399b1eb14f1f0f
xiangyang <2391747354@qq.com>
2022-04-29 23:08:00 +0800

    first

---
+++ zero/Main.java
@@ -0,0 +1,8 @@
+package com.company.zero;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ zero/soft.java
@@ -0,0 +1,11 @@
+package com.company.zero;
+
+public class soft {
+    public static void main(String[] args) {
+        long sum = 1;
+        for (int i = 1; i < 100; i++) {
+           sum *= i;
+        }
+        System.out.println(sum);
+    }
+}
